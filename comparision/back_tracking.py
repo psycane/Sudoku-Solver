d8b340c72accaf53b8a2d3a1b1764063ca8059e1
biggydbs <jain.hitesh30695@gmail.com>
2017-05-03 22:08:11 +0530

    Performance Analysis (added)

---
+++ comparision/back_tracking.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as mpatches
+
+
+l = [32, 40, 48, 108]
+
+N = 4
+
+
+ind = np.arange(N) 
+width = 0.35       
+
+rects1 = plt.bar(ind, l, width, color='b', edgecolor='black')
+axes = plt.gca()
+axes.set_ylim([0,120])
+axes.set_axisbelow(True)
+axes.yaxis.grid(color='black')
+axes.xaxis.grid(False)
+
+
+# add some text for labels, title and axes ticks
+plt.title('Back-Tracking')
+obj = ['Easy', 'Medium', 'Hard', 'Very\nHard']
+y_pos = np.arange(len(obj))
+plt.xticks(y_pos, obj)
+
+red_patch = mpatches.Patch(color='b', label='Back-Tracking')
+plt.legend(handles=[red_patch], bbox_to_anchor=(1.0, 0.5))
+
+plt.show()
